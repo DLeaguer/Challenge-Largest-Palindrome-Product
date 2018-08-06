2d4527b6f644c2edea8cc3d4e74444cfd4b73aff
DLeaguer <immostlikely@hotmail.com>
2018-08-05 17:01:16 -1000

    simpler setup, others are commented out & moved to the bottom

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -11,120 +11,174 @@ module.exports = function(digits){
   var palindromeNumber = 0;
   
   // do your work here
-  let result = [];
-  let answer = {};
+  
+  let highest = 0;
   
   console.log('');
   console.log('digits', digits);
-  console.log('result', result);
-
+  
   if (digits === 2) {
     for (let i=99; i>90; i--) {
       for (let j=i; j>90; j--) {
-        factor_0 = i; 
+        factor_0 = i;
         factor_1 = j;
         palindromeNumber = i * j;
         let pal = Number(palindromeNumber.toString().split('').reverse().join(''));
-        
-        // console.log('i', i, ' * ', j, ' is ', palindromeNumber, ' = ', pal, ' ?');
-        if (pal === palindromeNumber) {
-          result.push({
-            factor_0: i,
-            factor_1: j,
-            palindromeNumber: palindromeNumber
-          });
-          // console.log('');
-          // console.log('result');
-          // console.log(result);
-        }
-      }
-    } 
-    console.log('');
-    console.log('start loop through results')
-    for (let k in result) {
-      console.log('answer', answer);
-      console.log('result', result);
-      console.log('result[k].palindromeNumber', result[k].palindromeNumber);
-      console.log('answer.palindromeNumber', answer.palindromeNumber);
-      if (answer.palindromeNumber === undefined) {
-        answer = result[0];
-        console.log('1st answer', answer);
-      } else {
-        console.log(answer[k].palindromeNumber, '>', result[k].palindromeNumber, '?');
-        if (result[k].palindromeNumber > answer.palindromeNumber) {
-          answer = result;
-          console.log('');
-          console.log('factor_0 =', factor_0);
-          console.log('factor_1 =', factor_1);
-          console.log('palindromeNumber', palindromeNumber);
-          console.log('result =', result);
-          console.log('answer', answer);
+        // console.log(pal, '=', palindromeNumber, '?');
+        if(pal === palindromeNumber) {
+          if (highest < palindromeNumber) {
+            highest = palindromeNumber;
+            console.log(factor_0, '*', factor_1, '=', palindromeNumber);
+          }
+          return {
+            factor_0 : factor_0,
+            factor_1 : factor_1,
+            palindromeNumber : palindromeNumber
+          }
         }
       }
     }
-    return answer;
-
-
-  } else if (digits === 3) {
+  }
+  
+  else if (digits === 3) {
     for (let i=999; i>900; i--) {
       for (let j=i; j>900; j--) {
-        factor_0 = i; 
+        factor_0 = i;
         factor_1 = j;
         palindromeNumber = i * j;
         let pal = Number(palindromeNumber.toString().split('').reverse().join(''));
-        
-        // console.log('i', i, ' * ', j, ' is ', palindromeNumber, ' = ', pal, ' ?');
-        if (pal === palindromeNumber) {
-          result.push({
-            factor_0: i,
-            factor_1: j,
-            palindromeNumber: palindromeNumber
-          });
-          // console.log('');
-          // console.log('result');
-          // console.log(result);
-        }
-      }
-    } 
-    console.log('');
-    console.log('start loop through results')
-    for (let k in result) {
-      if (answer.palindromeNumber === undefined) {
-        answer = result[0];
-        console.log('');
-        console.log('answer', answer);
-        console.log('');
-        console.log('result');
-        console.log(result);
-        console.log('');
-        console.log('result[k].palindromeNumber', result[k].palindromeNumber);
-        console.log('answer.palindromeNumber', answer.palindromeNumber);
-        console.log('');
-        console.log('1st answer', answer);
-      } else {
-        console.log('');
-        console.log('other answers');
-        console.log(answer.palindromeNumber, '>', result[k].palindromeNumber, '?');
-        if (result[k].palindromeNumber > answer.palindromeNumber) {
-          answer = result[k];
-          console.log('');
-          console.log('factor_0 =', factor_0);
-          console.log('factor_1 =', factor_1);
-          console.log('palindromeNumber', palindromeNumber);
-          console.log('result =', result);
-          console.log('answer', answer);
+        // console.log(pal, '=', palindromeNumber, '?');
+        if(pal === palindromeNumber) {
+          if (highest < palindromeNumber) {
+            highest = palindromeNumber;
+            console.log(factor_0, '*', factor_1, '=', palindromeNumber);
+          }
+          return {
+            factor_0 : factor_0,
+            factor_1 : factor_1,
+            palindromeNumber : palindromeNumber
+          }
         }
       }
     }
-    return answer;
   }
-
-  return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
-  };
 };
+  
+  
+
+// push all palindrome objects to result array, loop through result & compare result[k].palindrome to answer.palindrome for higher amount
+
+// let result = [];
+// let answer = {};
+// console.log('result', result);
+
+// if (digits === 2) {
+  //   for (let i=99; i>90; i--) {
+    //     for (let j=i; j>90; j--) {
+      //       factor_0 = i; 
+      //       factor_1 = j;
+      //       palindromeNumber = i * j;
+      //       let pal = Number(palindromeNumber.toString().split('').reverse().join(''));
+      
+      //       // console.log('i', i, ' * ', j, ' is ', palindromeNumber, ' = ', pal, ' ?');
+      //       if (pal === palindromeNumber) {
+        //         result.push({
+          //           factor_0: i,
+          //           factor_1: j,
+  //           palindromeNumber: palindromeNumber
+  //         });
+  //         // console.log('');
+  //         // console.log('result');
+  //         // console.log(result);
+  //       }
+  //     }
+  //   } 
+  //   console.log('');
+  //   console.log('start loop through results')
+  //   for (let k in result) {
+  //     console.log('answer', answer);
+  //     console.log('result', result);
+  //     console.log('result[k].palindromeNumber', result[k].palindromeNumber);
+  //     console.log('answer.palindromeNumber', answer.palindromeNumber);
+  //     if (answer.palindromeNumber === undefined) {
+  //       answer = result[0];
+  //       console.log('1st answer', answer);
+  //     } else {
+  //       console.log(answer[k].palindromeNumber, '>', result[k].palindromeNumber, '?');
+  //       if (result[k].palindromeNumber > answer.palindromeNumber) {
+  //         answer = result;
+  //         console.log('');
+  //         console.log('factor_0 =', factor_0);
+  //         console.log('factor_1 =', factor_1);
+  //         console.log('palindromeNumber', palindromeNumber);
+  //         console.log('result =', result);
+  //         console.log('answer', answer);
+  //       }
+  //     }
+  //   }
+  //   return answer;
+  // } else if (digits === 3) {
+  //   for (let i=999; i>900; i--) {
+  //     for (let j=i; j>900; j--) {
+  //       factor_0 = i; 
+  //       factor_1 = j;
+  //       palindromeNumber = i * j;
+  //       let pal = Number(palindromeNumber.toString().split('').reverse().join(''));
+        
+  //       // console.log('i', i, ' * ', j, ' is ', palindromeNumber, ' = ', pal, ' ?');
+  //       if (pal === palindromeNumber) {
+  //         result.push({
+  //           factor_0: i,
+  //           factor_1: j,
+  //           palindromeNumber: palindromeNumber
+  //         });
+  //         // console.log('');
+  //         // console.log('result');
+  //         // console.log(result);
+  //       }
+  //     }
+  //   } 
+  //   console.log('');
+  //   console.log('start loop through results')
+  //   for (let k in result) {
+  //     if (answer.palindromeNumber === undefined) {
+  //       answer = result[0];
+  //       console.log('');
+  //       console.log('answer', answer);
+  //       console.log('');
+  //       console.log('result');
+  //       console.log(result);
+  //       console.log('');
+  //       console.log('result[k].palindromeNumber', result[k].palindromeNumber);
+  //       console.log('answer.palindromeNumber', answer.palindromeNumber);
+  //       console.log('');
+  //       console.log('1st answer', answer);
+  //     } else {
+  //       console.log('');
+  //       console.log('other answers');
+  //       console.log(answer.palindromeNumber, '>', result[k].palindromeNumber, '?');
+  //       if (result[k].palindromeNumber > answer.palindromeNumber) {
+  //         answer = result[k];
+  //         console.log('');
+  //         console.log('factor_0 =', factor_0);
+  //         console.log('factor_1 =', factor_1);
+  //         console.log('palindromeNumber', palindromeNumber);
+  //         console.log('result =', result);
+  //         console.log('answer', answer);
+  //       }
+  //     }
+  //   }
+  //   return answer;
+  // }
+
+  // return {
+  //   factor_0 : factor_0,
+  //   factor_1 : factor_1,
+  //   palindromeNumber : palindromeNumber
+  // };
+
+
+
 
 // this didn't work.  I was trying to make only one nested for loop for all digits.length
 
